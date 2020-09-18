0948f2aa842393ade399fc7f0078fe4a2de25c88
Victor Kurguzov <dolucky@yandex.by>
2020-09-18 15:19:12 +0300

    fix error

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,12 +23,37 @@ data "template_file" "user_data" {
   }
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["146.120.13.77/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_victor"
+  }
+}
+
 resource "aws_instance" "ec2_runner" {
   instance_type          = "t3.micro"
   user_data              = data.template_file.user_data.rendered
   ami                    = data.aws_ami.amazon_linux.id
   subnet_id              = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids = ["sg-03ee1a125a9791874"]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = "airflow-key"
   monitoring             = false
 
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "cbi-infra"
+    key    = "github-actions-demo.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
